e725484337f35a78acf415f48595c6bfc4f9536d
894risper <zilper894@gmail.com>
2024-11-01 18:43:09 +0300

    added a toaster

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-
+import { Toaster } from 'react-hot-toast';
 import './App.css'
 
 import  {BrowserRouter,Routes,Route} from "react-router-dom"
@@ -8,7 +8,8 @@ import Contacts from './pages/Contacts';
 import NoPage from './pages/NoPage';
 function App() {
 return (
-
+<div >
+<Toaster position="top-center" reverseOrder={false} />
   <BrowserRouter>
   <Routes>
     <Route path='/' element={<Layout/>}>
@@ -22,7 +23,7 @@ return (
   
   </BrowserRouter>
 
-   
+  </div> 
   )
 }
 
--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -1,53 +1,73 @@
 import React from 'react'
-import {useForm} from "react-hook-form"
-
-
+import { useForm } from "react-hook-form"
+import toast from "react-hot-toast"
 
 const Contacts = () => {
-  const {register,handleSubmit, formState,reset}=useForm()
-  const handleFormSubmit=(data)=>{
-   
+  const { register, handleSubmit, formState, reset } = useForm()
 
-    reset()
+  const handleFormSubmit = (data) => {
+    // Trigger notification on successful submission
+    toast("Form submitted successfully!")
+    reset() // Reset the form fields
   }
+
   return (
     <div>
       <h1>Contact Us</h1>
-<form onSubmit={handleSubmit(handleFormSubmit)}>
-  <div>
-    <label htmlFor="fname">First Name</label>
-    <input type="text"
-    placeholder='firstName'
-    {...register("fname",{
-      minLength:{
-        value:3,
-      message:"fname should be at least 3 characters ",
-    },
-    required: "tittleis required",
-pattern:{
-  value: /^[a-zA-Z ]+$/,
-  message: "Title should only contain letters", 
-}
-    })}
-    />
-    {formState.errors.fname &&(
-      <p className='text-sm text-red-500'>
-        {formState.errors.fname.message}
-      </p>
-    )}
-
-  </div>
-
-  <button
-                    className="p-4 py-2 mt-4 text-white bg-blue-800 rounded-md"
-                    type="submit"
-                >
-                    Submit
-                </button>
-</form>
-      
-
-
+      <form onSubmit={handleSubmit(handleFormSubmit)}>
+        <div>
+          <label htmlFor="fname">First Name</label>
+          <input
+            type="text"
+            placeholder='First Name'
+            {...register("fname", {
+              minLength: {
+                value: 3,
+                message: "First name should be at least 3 characters"
+              },
+              required: "First name is required",
+              pattern: {
+                value: /^[a-zA-Z ]+$/,
+                message: "First name should only contain letters"
+              }
+            })}
+          />
+          {formState.errors.fname && (
+            <p className='text-sm text-red-500'>
+              {formState.errors.fname.message}
+            </p>
+          )}
+        </div>
+        <div>
+          <label htmlFor="lname">Last Name</label>
+          <input
+            type="text"
+            placeholder='Last Name'
+            {...register("lname", {
+              minLength: {
+                value: 5,
+                message: "Last name should be at least 5 characters"
+              },
+              required: "Last name is required",
+              pattern: {
+                value: /^[a-zA-Z]+$/,
+                message: "Last name should only contain letters"
+              }
+            })}
+          />
+          {formState.errors.lname && (
+            <p className='text-sm text-red-500'>
+              {formState.errors.lname.message}
+            </p>
+          )}
+        </div>
+        <button
+          className="p-4 py-2 mt-4 text-white bg-blue-800 rounded-md"
+          type="submit"
+        >
+          Submit
+        </button>
+      </form>
     </div>
   )
 }
